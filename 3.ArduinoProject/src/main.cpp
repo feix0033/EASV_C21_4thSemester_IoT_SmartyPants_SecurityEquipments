9212d52b99744252c3b043190d9029e41c034141
feix0033 <feix0033@easv365.dk>
2023-02-23 14:46:49 +0100

    git init and example

---
+++ 3.ArduinoProject/src/main.cpp
@@ -0,0 +1,53 @@
+#include "Arduino.h"
+/*
+    Arduino with PIR motion sensor
+    For complete project details, visit: http://RandomNerdTutorials.com/pirsensor
+    Modified by Rui Santos based on PIR sensor by Limor Fried
+*/
+
+int led = 13;                // the pin that the LED is atteched to
+int sensor = 2;              // the pin that the sensor is atteched to
+int state = LOW;             // by default, no motion detected
+int val = 0;                 // variable to store the sensor status (value)
+const int buzzer = 9; //buzzer to arduino pin 9
+
+
+void setup() {
+    pinMode(led, OUTPUT);      // initalize LED as an output
+    pinMode(sensor, INPUT);    // initialize sensor as an input
+    Serial.begin(9600);        // initialize serial
+}
+
+void loop(){
+}
+
+void buzzerCode(){
+        tone(buzzer, 1000); // Send 1KHz sound signal...
+        delay(1000);        // ...for 1 sec
+        noTone(buzzer);     // Stop sound...
+        delay(1000);        // ...for 1sec
+}
+
+
+void motionSensorCode(){
+    val = digitalRead(sensor);   // read sensor value
+    if (val == HIGH) {           // check if the sensor is HIGH
+        digitalWrite(led, HIGH);   // turn LED ON
+        delay(100);                // delay 100 milliseconds
+
+        if (state == LOW) {
+            Serial.println("Motion detected!");
+            state = HIGH;       // update variable state to HIGH
+        }
+    }
+    else {
+        digitalWrite(led, LOW); // turn LED OFF
+        delay(200);             // delay 200 milliseconds
+
+        if (state == HIGH){
+            Serial.println("Motion stopped!");
+            state = LOW;       // update variable state to LOW
+        }
+    }
+}
+
