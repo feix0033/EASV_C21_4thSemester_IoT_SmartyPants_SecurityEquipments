b89a17d3375fd40142e875e6fa6b97e9e0e6a592
feix0033 <feix0033@easv365.dk>
2023-03-07 15:13:34 +0100

    setup all sensor working

--- 3.ArduinoProject/examProject_ESP32_No1_Sensor/src/main.cpp
+++ 3.ArduinoProject/examProject_ESP32_No1_Sensor/src/main.cpp
@@ -1,10 +1,38 @@
 #include <Arduino.h>
+#include "WiFi.h"
+
+
+int ledPin = 2;
+int pirPin = 12;
+int soundPin = A0;
+
+int pirValue;
+uint16_t soundValue;
+
 void setup() {
-// write your initialization code here
-    Serial.begin(9600);
+    // Serial.
+    Serial.begin(115200);
+
+    pinMode(ledPin,OUTPUT);
+    digitalWrite(ledPin,LOW);
+
+    // Wifi Setup
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
 
 }
 
 void loop() {
-// write your code here
+    pirValue = digitalRead(pirPin);
+    soundValue = analogRead(soundPin);
+
+    if(pirValue == 1 || soundValue >= 2500){
+        Serial.println("you have been broke");
+        digitalWrite(ledPin,HIGH);
+        delay(500);
+        digitalWrite(ledPin,LOW);
+        delay(500);
+    }
 }
+
